2570c55d07681cc6e0fa57380b601d7e933d7e9a
amira2011 <shaikh.abidali@gmail.com>
2024-06-11 21:05:57 +0530

    Implemented Freezing

--- lib/deep_freeze.rb
+++ lib/deep_freeze.rb
@@ -3,6 +3,19 @@
 require_relative "deep_freeze/version"
 
 module DeepFreeze
-  class Error < StandardError; end
-  # Your code goes here...
+  def deep_freeze(obj)
+    case obj
+    when String, Numeric, Symbol, TrueClass, FalseClass, NilClass
+      obj.freeze
+    when Hash
+      obj.each {|k,v| deep_freeze(k); deep_freeze(v)}
+      obj.freeze
+    when Array
+      obj.each { |e| deep_freeze(e) }
+      obj.freeze
+    else
+      raise "Cant Deep Freeze Objects of Class #{obj.class}"
+    end
+
+  end
 end
--- spec/deep_freeze_spec.rb
+++ spec/deep_freeze_spec.rb
@@ -1,11 +1,45 @@
 # frozen_string_literal: true
 
 RSpec.describe DeepFreeze do
-  it "has a version number" do
-    expect(DeepFreeze::VERSION).not_to be nil
+  include DeepFreeze
+  it "freezes arrays" do
+    array = [1, 2, 3]
+    deep_freeze(array)
+    expect(array.frozen?).to be true
+    expect(array.all?(&:frozen?)).to be true
+  end
+  it "freezes hashes" do
+    hash = {a: 1, b: 2, c: {d: 3, e: 4}}
+    deep_freeze(hash)
+    expect(hash.frozen?).to be true
+    expect(hash.keys.all?(&:frozen?)).to be true
+    expect(hash.values.all?(&:frozen?)).to be true
+    expect(hash[:c].keys.all?(&:frozen?)).to be true
+  end
+  it "freezes strings" do
+    string = "string"
+    deep_freeze(string)
+    expect(string.frozen?).to be true
+  end
+  it "freezes symbols" do
+    symbol = :symbol
+    deep_freeze(symbol)
+    expect(symbol.frozen?).to be true
+  end
+  it "freezes true booleans" do
+    boolean = true
+    deep_freeze(boolean)
+    expect(boolean.frozen?).to be true
+  end
+  it "freezes false booleans" do
+    boolean = false
+    deep_freeze(boolean)
+    expect(boolean.frozen?).to be true
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "raises an error trying to freeze unsupported types" do
+    unsupported_type = Object.new
+    expect {deep_freeze(unsupported_type)}.to raise_error(RuntimeError)
   end
+
 end
